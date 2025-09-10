b1bc35f8a50b60f4d0ab6fcf77559dce81a0d743
iscss123 <iscss123@gmail.com>
2025-09-10 14:57:33 +0900

    0910 알고리즘 행렬, dp

--- Algorithm/src/practice/algorithm_0910/p1258_행렬찾기.java
+++ Algorithm/src/practice/algorithm_0910/p1258_행렬찾기.java
@@ -1,10 +1,69 @@
 package practice.algorithm_0910;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class p1258_행렬찾기 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= tc; t++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			int[][] map = new int[N + 2][N + 2];
+			
+			for(int y = 1; y <= N; y++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int x = 1; x <= N; x++) {
+					map[y][x] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			PriorityQueue<Matrix> matrix = new PriorityQueue<>();
+			for(int y = 1; y <= N; y++) {
+				for(int x = 1; x <= N; x++) {
+					if(map[y][x] == 0) continue;
+					if(!(map[y - 1][x] == 0 && map[y][x - 1] == 0)) continue;//배열 시작점 찾기
+					int row = 0; int col = 0;
+					while(map[y + row][x] != 0) row++;
+					while(map[y][x + col] != 0) col++;
+					matrix.offer(new Matrix(row, col));
+				}
+			}
+			
+			sb.append("#").append(t).append(" ").append(matrix.size()).append(" ");
+			
+			while(!matrix.isEmpty()) {
+				Matrix m = matrix.poll();
+				sb.append(m.row).append(" ").append(m.col).append(" ");
+			}
+			
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		br.close();
 	}
-
+	
+	static class Matrix implements Comparable<Matrix>{
+		int row, col, size;
+		
+		public Matrix(int m, int n) {
+			this.row = m;
+			this.col = n;
+			this.size = m * n;
+		}
+		
+		@Override
+		public int compareTo(Matrix o) {
+			int n = this.size - o.size;
+			if(n == 0) return this.row - o.row;
+			else return n;
+		}
+	}
 }
--- Algorithm/src/practice/algorithm_0910/p1260_화학물질2.java
+++ Algorithm/src/practice/algorithm_0910/p1260_화학물질2.java
@@ -3,6 +3,8 @@ package practice.algorithm_0910;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class p1260_화학물질2 {
@@ -15,7 +17,7 @@ public class p1260_화학물질2 {
 		for(int t = 1; t <= tc; t++) {
 			int N = Integer.parseInt(br.readLine());
 			
-			int[][] map = new int[N + 1][N + 1];
+			int[][] map = new int[N + 2][N + 2];
 			
 			for(int y = 1; y <= N; y++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,10 +26,24 @@ public class p1260_화학물질2 {
 				}
 			}
 			
+			ArrayList<int[]> matrix = new ArrayList<>();
+			for(int y = 1; y <= N; y++) {
+				for(int x = 1; x <= N; x++) {
+					if(map[y][x] == 0) continue;
+					if(!(map[y - 1][x] == 0 && map[y][x - 1] == 0)) continue;//배열 시작점 찾기
+					int row = 0; int col = 0;
+					while(map[y + row][x] != 0) row++;
+					while(map[y][x + col] != 0) col++;
+					matrix.add(new int[] {row, col});
+				}
+			}
+			
+			int L = matrix.size();
+			int[][] dp = new int[L][L];
+			for(int )
 			
 			sb.append("#").append(t).append(" ").append(0).append("\n");
 		}
-		
 		System.out.println(sb);
 		br.close();
 	}
