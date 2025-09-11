c9e4e009170f658954e175e19dc0fa2939a2d458
iscss123 <iscss123@gmail.com>
2025-09-11 17:57:37 +0900

    0911 swea 1248 공통조상

--- Algorithm/src/going/p1248_공통조상.java
+++ Algorithm/src/going/p1248_공통조상.java
@@ -3,10 +3,17 @@ package going;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class p1248_공통조상 {
+	
+	static int V, E, A, B, cnt;
+	static boolean[] visited;
+	static ArrayList<Integer>[] fromto;
+	static Queue<Integer> q;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,35 +22,53 @@ public class p1248_공통조상 {
 		
 		for(int t = 1; t <= tc; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int V = Integer.parseInt(st.nextToken());
-			int E = Integer.parseInt(st.nextToken());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
 			
-			//트리 생성
-			ArrayList<Integer>[] tree = new ArrayList[V + 1];
-			for(int v = 1; v <= V; v++) tree[v] = new ArrayList<>();
+			fromto = new ArrayList[V + 1];
+			for(int v = 1; v <= V; v++) fromto[v] = new ArrayList<>();
 			
-			//트리에 data 추가
 			st = new StringTokenizer(br.readLine());
 			for(int e = 0; e < E; e++) {
 				int parent = Integer.parseInt(st.nextToken());
 				int child = Integer.parseInt(st.nextToken());
-				tree[parent].add(child);
+				fromto[parent].add(child);
 			}
 			
-			//탐색
-			boolean[] visited = new boolean[V + 1];
+			int n = 0;
+			int minParent = 0;
+			for(int v = 1; v <= V; v++) {
+				bfs(v);
+				if(visited[A] && visited[B]) {
+					n = cnt;
+					minParent = v;
+				}
+			}
 			
-			
-			//서브트리수 확인
-			int cnt = 0;
-			for(int v = 1; v <= V; v++)
-				if(visited[v]) cnt++;
-			
-			sb.append("#").append(t).append(" ").append(0).append(" ").append(cnt).append("\n");
+			sb.append("#").append(t).append(" ").append(minParent).append(" ").append(n).append("\n");
 		}
 		System.out.println(sb);
 		br.close();
+	}
+	
+	static void bfs(int in) {
+		q = new ArrayDeque<>();
+		q.offer(in);
+		
+		visited = new boolean[V + 1];
+		cnt = 1;
+		
+		while(!q.isEmpty()) {
+			int parent = q.poll();
+			
+			for(int child : fromto[parent]) {
+				if(visited[child]) continue;
+				visited[child] = true;
+				q.offer(child);
+				cnt++;
+			}
+		}
 	}
 }
