bbd76ec877e62fa136fa2270f2a525d378c9dd8d
iscss123@gmail.com <iscss123@gmail.com>
2025-10-14 01:37:47 +0900

    1014 알고리즘 python 연습

---
+++ Algorithm/bj/py/p3096.py
@@ -0,0 +1,11 @@
+def hanoi(N, s, e) :
+    if(N == 1) : 
+        print(s, e)
+        return
+    hanoi(N - 1, s, 6 - (s + e))
+    print(s, e)
+    hanoi(N - 1, 6 - (s + e), e)
+    
+N = int(input())
+print(2 ** N - 1)
+hanoi(N, 1, 3)
